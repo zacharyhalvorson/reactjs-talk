61d2ba8ac959c680976d63b18454650d819b51c6
Zachary Halvorson <zacharyhalvorson@me.com>
2017-10-23 22:00:20 -0700

    Initial commit

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Modal = styled.div`
+	background-color: papayawhip;
+	padding: 1rem;
+	color: teal;
+`;
+
+export default Modal;
